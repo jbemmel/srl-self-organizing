79ffd9d41bee576ccd7fa037d18c3b60496f2d81
J vanBemmel <jvb127@gmail.com>
2021-04-13 17:13:32 -0500

    Provision BGP also for spines;

--- gnmic-configure-interface.sh
+++ gnmic-configure-interface.sh
@@ -30,29 +30,56 @@ cat > $temp_file << EOF
 }
 EOF
 
+# For now, assume that the interface is already added to the default network-instance; only update its IP address
 /sbin/ip netns exec srbase-mgmt /usr/local/bin/gnmic -a 127.0.0.1:57400 -u admin -p admin --skip-verify -e json_ietf set \
   --replace-path /interface[name=$INTF] --replace-file $temp_file
 exitcode=$?
-# For now, assume that the interface is already added to the default network-instance; only update its IP address
 
-if [[ "$PEER_IP" != "" ]]; then
-cat > $temp_file << EOF
-{
-  "admin-state": "enable",
-  "autonomous-system": $AS,
-  "router-id": "$ROUTER_ID",
-  "ebgp-default-policy": {
-    "import-reject-all": false,
-    "export-reject-all": false
+if [[ "$PEER_IP" == "*" ]]; then
+IFS='' read -r -d '' DYNAMIC_NEIGHBORS << EOF
+"dynamic-neighbors": {
+    "accept": {
+      "match": [
+        {
+          "prefix": "192.168.0.0/24",
+          "peer-group": "leaves",
+          "allowed-peer-as": [
+            "65001..65002"
+          ]
+        }
+      ]
+    }
   },
-  "group": [
+"group": [
+    {
+      "group-name": "leaves",
+      "admin-state": "enable"
+    }
+  ],
+EOF
+else
+IFS='' read -r -d '' DYNAMIC_NEIGHBORS << EOF
+"group": [
     {
       "group-name": "spines",
       "admin-state": "enable",
       "export-policy": "export-hosts",
       "peer-as": 65000
     }
-  ],
+],
+EOF
+fi
+
+cat > $temp_file << EOF
+{
+  "admin-state": "enable",
+  "autonomous-system": $AS,
+  "router-id": "$ROUTER_ID",
+  $DYNAMIC_NEIGHBORS
+  "ebgp-default-policy": {
+    "import-reject-all": false,
+    "export-reject-all": false
+  },
   "ipv4-unicast": {
     "multipath": {
       "max-paths-level-1": 4,
@@ -65,13 +92,6 @@ cat > $temp_file << EOF
       "max-paths-level-2": 4
     }
   },
-  "neighbor": [
-    {
-      "peer-address": "$PEER_IP",
-      "admin-state": "enable",
-      "peer-group": "spines"
-    }
-  ],
   "route-advertisement": {
     "rapid-withdrawal": true
   }
@@ -79,7 +99,19 @@ cat > $temp_file << EOF
 
 EOF
 /sbin/ip netns exec srbase-mgmt /usr/local/bin/gnmic -a 127.0.0.1:57400 -u admin -p admin --skip-verify -e json_ietf set \
-  --update-path /network-instance[name=default]/protocols/bgp --update-file $temp_file
+  --replace-path /network-instance[name=default]/protocols/bgp --replace-file $temp_file
+exitcode+=$?
+
+if [[ "$PEER_IP" != "*" ]]; then
+cat > $temp_file << EOF
+{
+  "admin-state": "enable",
+  "peer-group": "spines"
+}
+EOF
+/sbin/ip netns exec srbase-mgmt /usr/local/bin/gnmic -a 127.0.0.1:57400 -u admin -p admin --skip-verify -e json_ietf set \
+  --update-path /network-instance[name=default]/protocols/bgp/neighbor[peer-address=$PEER_IP] --update-file $temp_file
+exitcode+=$?
 fi
 
 rm -f $temp_file
