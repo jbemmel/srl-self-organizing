84f1d9c70c16467d97de2f46757c5560ea08d9f5
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-11-20 21:35:53 +0000

    Do provision EVPN

--- src/auto-config-agent/auto-config-agent.py
+++ src/auto-config-agent/auto-config-agent.py
@@ -490,6 +490,44 @@ def Convert_to_lag(state,port,ip,vrf="overlay"):
 
      # bridge-table { mac-learning: { age-time: 300 } } leave as default value
    }
+   if state.evpn != 'disabled':
+      mac_vrf.update(
+      {
+        "vxlan-interface": [ { "name": f"vxlan0.0" } ],
+        "protocols": {
+         "bgp-evpn": {
+          "srl_nokia-bgp-evpn:bgp-instance": [
+           {
+             "id": 1,
+             "admin-state": "enable",
+             "vxlan-interface": f"vxlan0.0",
+             "evi": VNI_EVI, # Range 1..65535, cannot match VLAN 0 (untagged)
+             "ecmp": 8,
+             #"routes": {
+             # "bridge-table": {
+             #    "mac-ip": {
+             #      "advertise": False # Avoid duplicate IPs on links
+             #    }
+             #  }
+             # }
+           }
+          ]
+         },
+         "srl_nokia-bgp-vpn:bgp-vpn": {
+           "bgp-instance": [
+            { "id": 1,
+              # "export-policy": f"add-rt-{state.base_as}-{port}",
+              #"route-target": {
+              # "_annotate": "Need to specify explicitly, each leaf has a different AS so auto-RT won't work",
+              # "export-rt": rt,
+              # "import-rt": rt
+              #}
+            }
+           ]
+         }
+        }
+      })
+
    if state.evpn != "l2_only_leaves":
       mac_vrf['interface'] += [ { "name" : "irb0.0" } ]
 
@@ -503,44 +541,6 @@ def Convert_to_lag(state,port,ip,vrf="overlay"):
          # table.
          IRB_ARP_ND_TIMEOUT = 300 - 30
          ANNOTATION = "30 seconds lower than age-time in mac-vrf, to avoid transient packet loss when MAC address of ARP/ND entry is removed"
-
-         mac_vrf.update(
-         {
-           "vxlan-interface": [ { "name": f"vxlan0.0" } ],
-           "protocols": {
-            "bgp-evpn": {
-             "srl_nokia-bgp-evpn:bgp-instance": [
-              {
-                "id": 1,
-                "admin-state": "enable",
-                "vxlan-interface": f"vxlan0.0",
-                "evi": VNI_EVI, # Range 1..65535, cannot match VLAN 0 (untagged)
-                "ecmp": 8,
-                #"routes": {
-                # "bridge-table": {
-                #    "mac-ip": {
-                #      "advertise": False # Avoid duplicate IPs on links
-                #    }
-                #  }
-                # }
-              }
-             ]
-            },
-            "srl_nokia-bgp-vpn:bgp-vpn": {
-              "bgp-instance": [
-               { "id": 1,
-                 # "export-policy": f"add-rt-{state.base_as}-{port}",
-                 #"route-target": {
-                 # "_annotate": "Need to specify explicitly, each leaf has a different AS so auto-RT won't work",
-                 # "export-rt": rt,
-                 # "import-rt": rt
-                 #}
-               }
-              ]
-            }
-           }
-         })
-
       else:
          IRB_ARP_ND_TIMEOUT = 300
          ANNOTATION = "Avoid prolonged flooding due to MAC expiration (no EVPN triggered learning)"
