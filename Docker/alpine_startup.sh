c9a3e4a0971b6d1df22d157e3591db91114c3d0c
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-11-19 18:56:53 -0600

    Need to wait for interfaces to come online

--- Docker/alpine_startup.sh
+++ Docker/alpine_startup.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# Wait for interfaces to exist
+for i in $(seq 0 $CLAB_INTFS); do
+until [ -f /sys/class/net/eth${i}/carrier ];
+do
+  echo "Waiting for /sys/class/net/eth${i}/carrier"
+  sleep 1
+done
+done
+echo "All $CLAB_INTFS + eth0 are up!"
+
 # Bring up network interfaces
 ifup -a
 
