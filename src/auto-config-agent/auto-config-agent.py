b7f2e180b91a0281804d19b01f6f1dd4e64f9a92
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-11-24 12:24:49 -0600

    Need to configure lag speed

--- src/auto-config-agent/auto-config-agent.py
+++ src/auto-config-agent/auto-config-agent.py
@@ -478,7 +478,7 @@ def Convert_to_lag(state,port,ip,peer_data,vrf):
       ],
       "lag": {
        "lag-type": "static", # May get upgraded to LACP in case of MC-LAG
-       # "member-speed": "100G" # 25G not supported on spines, 100G not on leaves
+       "member-speed": "25G" if state.bridging_supported else "100G" # 25G not supported on spines, 100G not on leaves
       }
    }
 
