da659b4f8e5b92619f06271d6876e4bca4d68ee8
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2022-06-02 21:17:32 -0500

    Fix EBGP peering from leaf in case of L2-only evpn

--- src/auto-config-agent/auto-config-agent.py
+++ src/auto-config-agent/auto-config-agent.py
@@ -1380,7 +1380,7 @@ def configure_peer_link( state, intf_name, lldp_my_port, lldp_peer_port,
          intf_name, # Name of the interface being provisioned
          _ip,
          lldp_peer_desc,
-         _peer if ((peer_type=='spine' and _r==1 and state.evpn!="l2_only_leaves")
+         _peer if ((peer_type=='spine' and _r==1)
                    or leaf_pair_link) else '*', # Only when connecting "upwards"
          state.router_id if set_router_id else "*",
          min_peer_as, # For spine, allow both iBGP (same AS) and eBGP
