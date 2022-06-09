e8264d7ccfdd6611ae7dfaec718532a96b492c7c
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2022-06-09 04:52:32 -0500

    Use spines as RR

---
+++ labs/spine-leaf-evpn/delta-auto-config.cmd
@@ -0,0 +1,13 @@
+set /system gnmi-server unix-socket admin-state enable
+set /auto-config-agent gateway ipv4 10.0.0.1/24 location spine
+set /auto-config-agent lacp active # reload-delay-secs 0
+set /auto-config-agent igp bgp evpn model l2-only-leaves auto-lags encoded-ipv6 bgp-peering ipv4 overlay-as 65000 route-reflector spine
+
+# Test ipv6 underlay
+# set /auto-config-agent evpn ipv6-nexthops true
+
+# NEW: Service config
+set /auto-config-agent service 1 name "Boot" vlan 0 l3 gateway anycast-gw-on-leaves gateway-ipv4 10.0.0.1/24
+
+# Test DHCP tracing - not working
+set /interface mgmt0 subinterface 0 ipv4 dhcp-client trace-options trace [messages]
