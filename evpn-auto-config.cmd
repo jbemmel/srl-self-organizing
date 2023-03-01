568d6a4cbcb21cc200601caf366b99f828a851cf
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2023-03-01 15:36:43 -0600

    Use ipv4 for iBGP overlay

--- evpn-auto-config.cmd
+++ evpn-auto-config.cmd
@@ -1,5 +1,4 @@
 # auto-config for srl-evpn lab leaves & spines
 set /system gnmi-server unix-socket admin-state enable
+set /auto-config-agent igp bgp-unnumbered evpn model asymmetric-irb auto-lags encoded-ipv6 bgp-peering ipv4
 set /auto-config-agent gateway ipv4 10.0.0.1/24
-set /auto-config-agent lacp active lacp-fallback 90 # reload-delay-secs 0
-set /auto-config-agent igp ospf evpn model asymmetric-irb auto-lags encoded-ipv6 bgp-peering ipv4 overlay-as 65000
