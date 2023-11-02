585ca04e1ec7bc0388a502e0bec3996fd1c63ab7
Jeroen van Bemmel <jvb127@gmail.com>
2023-11-02 20:37:57 +0000

    Add venv path

--- src/auto-config-agent/scripts/auto-config-agent.sh
+++ src/auto-config-agent/scripts/auto-config-agent.sh
@@ -25,10 +25,11 @@ function main()
     VENV_LIB="${virtual_env}/lib/python3.6/site-packages"
 
     NDK="/opt/rh/rh-python36/root/usr/lib/python3.6/site-packages/sdk_protos"
+    LIB="/opt/srlinux/python/virtual-env/lib/python3.6/site-packages/"
     # since 21.6
     SDK2="/usr/lib/python3.6/site-packages/sdk_protos"
     APP="/opt/demo-agents/auto-config-agent"
-    export PYTHONPATH="$VENV_LIB:$NDK:$SDK2:$APP:$PYTHONPATH"
+    export PYTHONPATH="$VENV_LIB:$LIB:$NDK:$SDK2:$APP:$PYTHONPATH"
 
     export http_proxy=""
     export https_proxy=""
