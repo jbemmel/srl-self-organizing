d7fe4da5a1b6478c35fbf2dfc7286dfbdde11eed
Jeroen van Bemmel <jvb127@gmail.com>
2024-03-29 16:36:37 +0000

    Updated for 24.3

--- labs/spine-leaf-evpn/delta-auto-config.cmd
+++ labs/spine-leaf-evpn/delta-auto-config.cmd
@@ -1,8 +1,8 @@
 # set /auto-config-agent gateway ipv4 10.0.{port}.1/24
 
 # auto-config for srl-evpn lab leaves & spines
-set /system gnmi-server unix-socket admin-state enable use-authentication false
-set /system gnmi-server rate-limit 65000
+set /system grpc-server mgmt unix-socket admin-state enable
+set /system grpc-server mgmt rate-limit 65000
 set /auto-config-agent igp isis-unnumbered evpn model symmetric-irb auto-lags encoded-ipv6 bgp-peering ipv4 ebgp true
 set /auto-config-agent lacp active # reload-delay-secs 0
 
